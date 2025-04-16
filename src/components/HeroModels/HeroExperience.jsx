a2789eb5a83ee7ab67554d598bb21b125a99c7a6
hemant <tyagi26326@gmail.com>
2025-04-16 10:51:06 +0530

    added lighting to the scene

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -2,6 +2,7 @@ import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Room } from "./Optimized-room"
 import { useMediaQuery } from "react-responsive"
+import HeroLights from "./HeroLights"
 
 const HeroExperience = () => {
 
@@ -10,8 +11,6 @@ const HeroExperience = () => {
 
   return (
     <Canvas camera={{position: [0, 0, 15], fov: 45 }}>
-      <ambientLight intensity={1} color="#1a1a40"/>
-      <directionalLight position={[5,5,5]} intensity={2}/>
       <OrbitControls
         enablePan={false}
         enableZoom={false}
@@ -20,6 +19,7 @@ const HeroExperience = () => {
         minPolarAngle={Math.PI / 5}
         maxPolarAngle={Math.PI / 2}
       />
+      <HeroLights/>
       <group
        scale={isMobile? 0.7: 1}
         position={[0, -3.5, 0]}
