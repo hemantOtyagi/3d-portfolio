4b09ef3b9b5028f6e0afadbddce4c9edc2b6e141
hemant <tyagi26326@gmail.com>
2025-04-17 09:48:27 +0530

    added featured card showcasing my strength

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import LogoSection from "./components/LogoSection"
 import NavBar from "./components/NavBar"
+import FeatureCards from "./components/sections/FeatureCards"
 import Hero from "./components/sections/Hero"
 import ShowcaseSections from "./components/sections/ShowcaseSections"
 
@@ -8,6 +10,8 @@ const App = () => {
       <NavBar/>
       <Hero/>
       <ShowcaseSections/>
+      <LogoSection/>
+      <FeatureCards/>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,24 @@
 import { navLinks } from "../constants"
+import { useState, useEffect } from 'react'
 
 const NavBar = () => {
+   
+  const [scrolled , setScrolled] = useState(false);
+
+  useEffect(() => {
+     
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      setScrolled(true);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+
+  }, [])
+
   return (
-    <header className="navbar">
+    <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
       <div className="inner">
         <a className="logo" href="#hero">
           Hemant 
